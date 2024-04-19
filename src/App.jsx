1a3e3f7ec4b1721a14ba3ddef261a643f3a44d1e
Mohick <haokietks7@gmail.com>
2024-04-20 00:58:31 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div id='app__box--the-bai'>
+        <div className='app__the--bai'>
+          <div className='app__the__bai--mat-truoc'></div>
+          <div className='app__the__bai--mat-sau'></div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default App
